7256b6203035bf1ad44ef6b8dec183db6fa75cef
Adam Tropp <adt6261@gmail.com>
2018-02-07 20:17:20 +0000

    got rid of nonsensical nesting of recurring payments in bank account creation form

--- src/actions/bankActions.js
+++ src/actions/bankActions.js
@@ -1,10 +1,7 @@
 import fetch from 'isomorphic-fetch'
 
 export function addBankAccount(account){
-    var data = {name: account.name, balance: account.balance, recurring_payments:
-       {account.recurring_payments.map((payment) => source: payment.source, status: payment.status,
-       pay_date: payment.pay_date, pay_amount: payment.pay_amount, duration: payment.duration,
-     payment_frequency: payment.payment_frequency)}};
+    var data = {name: account.name, balance: account.balance};
     return function(dispatch){
         dispatch({type: 'ADD_BANK_ACCOUNT'})
         return fetch('/api/bank_accounts', {
