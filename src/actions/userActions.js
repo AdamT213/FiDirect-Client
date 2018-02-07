4eea122882a21a1feee9f29c580a132634ede598
Adam Tropp <adt6261@gmail.com>
2018-02-07 19:15:07 +0000

    changes url of request to include api scoping

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,27 +1,27 @@
-import fetch from 'isomorphic-fetch' 
+import fetch from 'isomorphic-fetch'
 
-export function signUpUser(user){ 
+export function signUpUser(user){
     var data = {email: user.email, password: user.password};
     return function(dispatch){
         dispatch({type: 'SIGN_UP_USER'})
-        return fetch('http://localhost:3000/users/sign_up', {
-        method: 'POST', 
-        body: JSON.stringify(data), 
+        return fetch('/api/users/sign_up', {
+        method: 'POST',
+        body: JSON.stringify(data),
         }).then(res => res.json())
         .catch(error => console.error('Error:', error))
         .then(response => console.log('Success:', response));
-    } 
-} 
+    }
+}
 
-export function signInUser(user){ 
+export function signInUser(user){
     var data = {email: user.email, password: user.password};
-    return function(dispatch){ 
+    return function(dispatch){
         dispatch({type: 'SIGN_IN_USER'})
-        return fetch('http://localhost:3000/users/sign_in', {
-        method: 'POST', 
-        body: JSON.stringify(data), 
+        return fetch('/api/users/sign_in', {
+        method: 'POST',
+        body: JSON.stringify(data),
         }).then(res => res.json())
         .catch(error => console.error('Error:', error))
         .then(response => console.log('Success:', response));
-    } 
-} 
+    }
+}
