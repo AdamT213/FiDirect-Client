5ddc35909bf377b0fd287bb5cc49c8931f97b4f2
Adam Tropp <adt6261@gmail.com>
2018-02-07 19:18:38 +0000

    added application json header to requests

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -6,6 +6,9 @@ export function signUpUser(user){
         dispatch({type: 'SIGN_UP_USER'})
         return fetch('/api/users/sign_up', {
         method: 'POST',
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
         body: JSON.stringify(data),
         }).then(res => res.json())
         .catch(error => console.error('Error:', error))
@@ -19,6 +22,9 @@ export function signInUser(user){
         dispatch({type: 'SIGN_IN_USER'})
         return fetch('/api/users/sign_in', {
         method: 'POST',
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
         body: JSON.stringify(data),
         }).then(res => res.json())
         .catch(error => console.error('Error:', error))
