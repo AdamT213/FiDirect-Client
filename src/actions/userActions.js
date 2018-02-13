190d7358f5d15da48186b45398f7d0602979a4b2
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-13 19:51:48 +0000

    added rack cors and changed api endpoint name, working on making fetch requests work

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -4,13 +4,13 @@ export function signUpUser(user){
     var data = {email: user.email, password: user.password, password_confirmation: user.password};
     return function(dispatch){
         dispatch({type: 'SIGN_UP_USER'})
-        return fetch('/api/users', {
+        return fetch('https://ee55715a523f4af8bae9f5467daf644d.vfs.cloud9.us-east-2.amazonaws.com:8081/api/users', {
         method: 'POST', 
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify({user: data}),
         }).then(res => console.log(res))
         .catch(error => console.error('Error:', error))
         .then(response => console.log('Success:', response));
