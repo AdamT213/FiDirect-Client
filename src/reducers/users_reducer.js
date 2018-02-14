8a3ee060dd31146d8579e585497a03d0e868c9da
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-14 22:22:35 +0000

    stubbed out ability to save user id

--- src/reducers/users_reducer.js
+++ src/reducers/users_reducer.js
@@ -1,11 +1,14 @@
-export function usersReducer(state = {currentUser: {}}, action) {
+export function usersReducer(state = {currentUser: {}, user_id: null}, action) {
   switch (action.type) {
 
     case 'SIGN_UP_USER':
-      return Object.assign({}, state, {currentUser: {id: action.id, email: action.email, password: action.password }})
-    case 'SIGN_IN_USER':
-      debugger;
-      return Object.assign({}, state, {currentUser: {id: action.payload.id, email: action.payload.email, password: action.payload.password }})
+      window.localStorage.setItem('id', 'action.id')
+      return Object.assign({}, state, {currentUser: {id: action.id, email: action.email, password: action.password }, 
+      user_id: window.localStorage.getItem('id') }) 
+    case 'SIGN_IN_USER': 
+      window.sessionStorage.setItem('id', 'action.id')
+      return Object.assign({}, state, {currentUser: {id: action.payload.id, email: action.payload.email, password: action.payload.password }, 
+      user_id: window.sessionStorage.getItem('id') })
     default:
       return state
   }
