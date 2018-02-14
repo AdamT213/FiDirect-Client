39acfdba575502350592bd5a73e7364c348ee84a
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-14 22:42:10 +0000

    working on adding router to app component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css'; 
-import UserSignUpForm from './containers/UserSignUpForm' 
-import UserSignInForm from './containers/UserSignInForm' 
-import BankInput from './containers/BankInput' 
+import UserSignUpForm from './inputcontainers/UserSignUpForm' 
+import UserSignInForm from './inputcontainers/UserSignInForm' 
+import BankInput from './inputcontainers/BankInput' 
+import AssetInput from './inputcontainers/AssetInput'
+import CreditCardInput from './inputcontainers/CreditCardInput'
+import InvestmentInput from './inputcontainers/InvestmentInput'
+import LoanInput from './inputcontainers/LoanInput' 
+import RecurringPaymentInput from './inputcontainers/RecurringPaymentInput' 
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+  render() { 
+    if (this.state.user_id === null) { 
+      return (
+        <div className="App">
+          <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1> 
+          <h1 className="App-title">FiDirect</h1> 
           <UserSignUpForm/>
           <UserSignInForm/> 
-          <BankInput />
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+          </header>
+        </div>
+      );
+    } 
+    else { 
+      return (
+        <div className="App">
+          <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">FiDirect</h1> 
+          </header>
+        </div>
+      );
+   }
 }
 
 export default App;
