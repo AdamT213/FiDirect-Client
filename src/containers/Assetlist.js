344db29393caf34ebd611cdf27cd839eb3621653
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-14 23:36:59 +0000

    updated assetlist to handle nested routing

--- src/containers/Assetlist.js
+++ src/containers/Assetlist.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'; 
-import Asset from '../presentational/Asset';
+import { Route, Switch } from 'react-router-dom';
+import Asset from '../presentational/Asset'; 
+import AssetInput from '../inputcontainers/AssetInput' 
+import Assets from './Assets'
 
 export class Assetlist extends Component {
 
@@ -9,10 +12,19 @@ export class Assetlist extends Component {
       return <Asset name={asset.name} value= {asset.value} key={index} />
     });
 
-    return (
-        <ul> 
-         {assets} 
-        </ul>
+    return ( 
+        <div>
+          <Switch>
+            <Route path={`${this.props.match.url}/new`} component={AssetInput} />
+            <Route path={`${this.props.match.url}/getAssets`} component={Assets}/>
+            <Route exact path={this.props.match.url} render={() => (
+              <h3>Here are all of your Assets</h3>
+            )}/> 
+          </Switch>
+          <ul> 
+            {assets} 
+          </ul> 
+        </div>
     )
   }
 };
