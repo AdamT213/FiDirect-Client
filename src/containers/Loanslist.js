5a660fde8e453d6d4fb8c4b592095d5961ab24a1
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-14 23:52:40 +0000

    updated all presentational components to involve nested routing

--- src/containers/Loanslist.js
+++ src/containers/Loanslist.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'; 
+import { Route, Switch } from 'react-router-dom';
 import Loan from '../presentational/Loan';
+import LoanInput from '../inputcontainers/LoanInput' 
+import Loans from './Loans'
 
 export class Loanslist extends Component {
 
@@ -10,9 +13,18 @@ export class Loanslist extends Component {
     });
 
     return (
-        <ul> 
-         {loans} 
-        </ul>
+        <div>
+          <Switch>
+            <Route path={`${this.props.match.url}/new`} component={LoanInput} />
+            <Route path={`${this.props.match.url}/getInvestments`} component={Loans}/>
+            <Route exact path={this.props.match.url} render={() => (
+              <h3>Here are all of your Loans</h3>
+            )}/> 
+          </Switch>
+          <ul> 
+            {loans} 
+          </ul> 
+        </div>
     )
   }
 };
