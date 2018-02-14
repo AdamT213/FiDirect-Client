f07282ea66f5be5efe22e71ccb1d50cbcca77ba0
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-14 21:42:12 +0000

    stubbed out component to show a users assets

---
+++ src/containers/Assetlist.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'; 
+import Assets from './Assets' 
+import Asset from '../presentational/Asset'
+
+export class Assetlist extends Component {
+
+  render() {
+
+    const assets = this.props.store.getState().assets.map((asset, index) => {
+      return <Asset name={asset.name} value= {asset.value} key={index} />
+    });
+
+    return (
+        <ul> 
+         {assets} 
+        </ul>
+    )
+  }
+};
