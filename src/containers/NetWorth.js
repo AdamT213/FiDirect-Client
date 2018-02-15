c03296773dd90a51530228dd2c2a9723e3d26536
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-15 19:02:59 +0000

    stubbed oout networth component to acct as homepage

---
+++ src/containers/NetWorth.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'; 
+import calculateNetWorth from '../networthActions'; 
+
+class NetWorth extends Component {
+
+  componentDidMount() {
+    this.props.calculateNetWorth();
+  } 
+  
+  render() {
+    return (
+      <div>
+        <h2>Your current net worth is: {this.props.calculateNetWorth()}</h2>
+      </div>
+    )
+  }
+}; 
+
+const mapStateToProps = state => {
+  return {
+    networth: state.networth
+  };
+} 
+
+export default connect(mapStateToProps, { calculateNetWorth })(NetWorth);
