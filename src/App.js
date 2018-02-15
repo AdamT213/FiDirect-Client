e6e5a540af31cd0eae19cca1b78e63e2a0a82b3b
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-15 21:41:41 +0000

    working through errors preventing rendering in browser

--- src/App.js
+++ src/App.js
@@ -37,13 +37,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">FiDirect</h1> 
           <Router>
-            <NavBar />
-              <Route exact path="/" component={NetWorth} />
-              <Route exact path="/assets" component={Assetlist} />
-              <Route exact path="/bank_accounts" component={Bankslist} />
-              <Route exact path="/credit_cards" component={CreditCardslist} /> 
-              <Route exact path="/investments" component={Investmentslist} /> 
-              <Route exact path="/loans" component={Loanslist} />
+            <div>
+              <NavBar /> 
+                <Route exact path="/" component={NetWorth} />
+                <Route exact path="/assets" component={Assetlist} />
+                <Route exact path="/bank_accounts" component={Bankslist} />
+                <Route exact path="/credit_cards" component={CreditCardslist} /> 
+                <Route exact path="/investments" component={Investmentslist} /> 
+                <Route exact path="/loans" component={Loanslist} /> 
+              </div>
           </Router>
           </header>
         </div>
