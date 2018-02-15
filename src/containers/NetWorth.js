8e876cbe4e59f89ac61b5cb83f356dc453af359e
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-15 22:38:45 +0000

    got rid of experimental code in networth.js. working on passing state between reducers

--- src/containers/NetWorth.js
+++ src/containers/NetWorth.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; 
 import { calculateNetWorth } from '../actions/networthActions'; 
-import { getAssets } from '../actions/assetActions'
-import { getBanks } from '../actions/bankActions'
-import { getCards } from '../actions/cardActions' 
-import { getInvestments } from '../actions/investmentActions'
-import { getLoans } from '../actions/loanActions'
 
 class NetWorth extends Component {
 
   componentDidMount() {
-    this.props.getAssets();
-    this.props.getBanks(); 
-    this.props.getCards(); 
-    this.props.getInvestments(); 
-    this.props.getLoans();
     this.props.calculateNetWorth();
   } 
   
@@ -29,13 +19,8 @@ class NetWorth extends Component {
 
 const mapStateToProps = state => {
   return {
-    networth: state.networth,
-    assets: state.assets,
-    banks: state.banks,
-    cards: state.cards, 
-    investments: state.investments, 
-    loans: state.loans,
+    networth: state.networth
   };
 } 
 
-export default connect(mapStateToProps, { calculateNetWorth }, { getAssets }, { getBanks }, { getCards }, { getInvestments }, { getLoans })(NetWorth);
+export default connect(mapStateToProps, { calculateNetWorth })(NetWorth);
