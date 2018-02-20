8f6111d45a75d3d8e79c60349479a1abf784ed36
Adam Tropp <adt6261@gmail.com>
2018-02-20 05:44:31 +0000

    updated asssetlist to latest aws

--- src/containers/Assetlist.js
+++ src/containers/Assetlist.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react'; 
+import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom';
-import Asset from '../presentational/Asset'; 
-import AssetInput from '../inputcontainers/AssetInput' 
+import Asset from '../presentational/Asset';
+import AssetInput from '../inputcontainers/AssetInput'
 import Assets from './Assets'
 
-export class Assetlist extends Component {
+class Assetlist extends Component {
 
   render() {
 
-    const assets = this.props.store.getState().assets.map((asset, index) => {
+    const assets = this.props.assets.map((asset, index) => {
       return <Asset name={asset.name} value= {asset.value} key={index} />
     });
 
-    return ( 
+    return (
         <div>
           <Switch>
             <Route path={`${this.props.match.url}/new`} component={AssetInput} />
             <Route path={`${this.props.match.url}/getAssets`} component={Assets}/>
             <Route exact path={this.props.match.url} render={() => (
+              <div>
               <h3>Here are all of your Assets</h3>
-            )}/> 
+              <ul>
+                {assets}
+              </ul>
+              </div>
+            )}/>
           </Switch>
-          <ul> 
-            {assets} 
-          </ul> 
         </div>
     )
   }
 };
+
+function mapStateToProps(state){
+  return {assets: state.assetsReducer.assets}
+}
+
+export default connect(mapStateToProps)(Assetlist);
--- src/containers/NetWorth.js
+++ src/containers/NetWorth.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'; 
-import { calculateNetWorth } from '../actions/networthActions'; 
+import { connect } from 'react-redux';
+import { calculateNetWorth } from '../actions/networthActions';
 
 class NetWorth extends Component {
 
   componentDidMount() {
     this.props.calculateNetWorth();
-  } 
-  
+  }
+
   render() {
     return (
-      <div>
-        <h2>Your current net worth is ${this.props.calculateNetWorth()}</h2>
-      </div>
+      <h2>Your current net worth is ${this.props.calculateNetWorth()}</h2>
     )
   }
-}; 
+};
+
 
 const mapStateToProps = state => {
   return {
     networth: state.networth
   };
-} 
+}
 
 export default connect(mapStateToProps, { calculateNetWorth })(NetWorth);
