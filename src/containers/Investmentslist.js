8e866b35c5287d3ba73d86522cac12bc88a89c49
Adam Tropp <adt6261@gmail.com>
2018-02-20 05:52:20 +0000

    updated loanslist to latest aws

--- src/containers/Investmentslist.js
+++ src/containers/Investmentslist.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom';
 import Investment from '../presentational/Investment';
-import InvestmentInput from '../inputcontainers/InvestmentInput' 
+import InvestmentInput from '../inputcontainers/InvestmentInput'
 import Investments from './Investments'
 
-export class Investmentslist extends Component {
+class Investmentslist extends Component {
 
   render() {
 
-    const investments = this.props.store.getState().investments.map((investment, index) => {
+    const investments = this.props.investments.map((investment, index) => {
       return <Investment name={investment.name} value= {investment.value} payments= {investment.Payments} key={index} />
     });
 
@@ -18,13 +19,21 @@ export class Investmentslist extends Component {
             <Route path={`${this.props.match.url}/new`} component={InvestmentInput} />
             <Route path={`${this.props.match.url}/getInvestments`} component={Investments}/>
             <Route exact path={this.props.match.url} render={() => (
+            <div>
               <h3>Here are all of your Investments</h3>
-            )}/> 
+              <ul>
+                {investments}
+              </ul>
+              </div>
+            )}/>
           </Switch>
-          <ul> 
-            {investments} 
-          </ul> 
         </div>
     )
   }
 };
+
+function mapStateToProps(state){
+  return {investments: state.investmentsReducer.investments}
+}
+
+export default connect(mapStateToProps)(Investmentslist);
--- src/containers/Loanslist.js
+++ src/containers/Loanslist.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom';
 import Loan from '../presentational/Loan';
-import LoanInput from '../inputcontainers/LoanInput' 
+import LoanInput from '../inputcontainers/LoanInput'
 import Loans from './Loans'
 
-export class Loanslist extends Component {
+class Loanslist extends Component {
 
   render() {
 
-    const loans = this.props.store.getState().loans.map((loan, index) => {
+    const loans = this.props.loans.map((loan, index) => {
       return <Loan name={loan.name} balance= {loan.remaining_balance} interest= {loan.interest_rate} payments= {loan.Payments} key={index} />
     });
 
@@ -18,13 +19,21 @@ export class Loanslist extends Component {
             <Route path={`${this.props.match.url}/new`} component={LoanInput} />
             <Route path={`${this.props.match.url}/getInvestments`} component={Loans}/>
             <Route exact path={this.props.match.url} render={() => (
+            <div>
               <h3>Here are all of your Loans</h3>
-            )}/> 
+              <ul>
+                {loans}
+              </ul>
+              </div>
+            )}/>
           </Switch>
-          <ul> 
-            {loans} 
-          </ul> 
         </div>
     )
   }
 };
+
+function mapStateToProps(state){
+  return {loans: state.loansReducer.loans}
+}
+
+export default connect(mapStateToProps)(Loanslist);
