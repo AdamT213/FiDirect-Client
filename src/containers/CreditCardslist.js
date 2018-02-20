d9deea58da09966c68d7e202897c2f19eead83bb
Adam Tropp <adt6261@gmail.com>
2018-02-20 05:49:53 +0000

    updated investmentslist to latest aws

--- src/containers/CreditCardslist.js
+++ src/containers/CreditCardslist.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom';
 import CreditCard from '../presentational/CreditCard';
-import CreditCardInput from '../inputcontainers/CreditCardInput' 
+import CreditCardInput from '../inputcontainers/CreditCardInput'
 import CreditCards from './CreditCards'
 
-export class CreditCardslist extends Component {
+
+class CreditCardslist extends Component {
+
 
   render() {
 
-    const cards = this.props.store.getState().cards.map((card, index) => {
+    const cards = this.props.cards.map((card, index) => {
       return <CreditCard provider={card.provider} balance= {card.balance} interest= {card.interest_rate} payments= {card.Payments} key={index} />
     });
 
@@ -18,13 +21,21 @@ export class CreditCardslist extends Component {
             <Route path={`${this.props.match.url}/new`} component={CreditCardInput} />
             <Route path={`${this.props.match.url}/getCards`} component={CreditCards}/>
             <Route exact path={this.props.match.url} render={() => (
+              <div>
               <h3>Here are all of your Credit Cards</h3>
-            )}/> 
+              <ul>
+                {cards}
+              </ul>
+              </div>
+            )}/>
           </Switch>
-          <ul> 
-            {cards} 
-          </ul> 
         </div>
     )
   }
 };
+
+function mapStateToProps(state){
+  return {cards: state.cardsReducer.cards}
+}
+
+export default connect(mapStateToProps)(CreditCardslist);
