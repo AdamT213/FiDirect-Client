3931cce8b2013771765d0708e280e61a5661c9db
Adam Tropp <adt6261@gmail.com>
2018-02-24 19:34:33 +0000

    fixed sign in action to use user object and allow sign in

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,11 +1,11 @@
 import fetch from 'isomorphic-fetch'
 
-export function signUpUser(user){ 
+export function signUpUser(user){
     var data = {email: user.email, password: user.password, password_confirmation: user.password};
     return function(dispatch){
         dispatch({type: 'SIGN_UP_USER'})
         return fetch('https://ee55715a523f4af8bae9f5467daf644d.vfs.cloud9.us-east-2.amazonaws.com:8081/api/users', {
-        method: 'POST', 
+        method: 'POST',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json'
@@ -27,7 +27,7 @@ export function signInUser(user){
           Accept: 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify({user: data}),
         }).then(res => console.log(res))
         .catch(error => console.error('Error:', error))
         .then(response => console.log('Success:', response));
