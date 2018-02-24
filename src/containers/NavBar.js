f9e02c2b77b043eaa600f12160966edeafe38d3f
Adam Tropp <adt6261@gmail.com>
2018-02-24 20:28:25 +0000

    added spaced between navbar links

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -2,37 +2,37 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-  return ( 
+  return (
     <div className="navbar">
-      <NavLink 
+      <NavLink className="link"
       to="/"
       exact
-    >NetWorth</NavLink> 
-    <NavLink 
+    >NetWorth</NavLink>
+    <NavLink className="link"
       to="/assets"
       exact
-    >Assets</NavLink> 
-    <NavLink 
+    >Assets</NavLink>
+    <NavLink className="link"
       to="/bank_accounts"
       exact
-    >Bank Accounts</NavLink> 
-    <NavLink 
+    >Bank Accounts</NavLink>
+    <NavLink className="link"
       to="/credit_cards"
       exact
-    >Credit Cards</NavLink> 
-    <NavLink 
+    >Credit Cards</NavLink>
+    <NavLink className="link"
       to="/investments"
       exact
-    >Investments</NavLink>  
-    <NavLink 
+    >Investments</NavLink>
+    <NavLink className="link"
       to="/loans"
       exact
-    >Loans</NavLink>  
-    <NavLink 
+    >Loans</NavLink>
+    <NavLink className="link"
       to="/recurring_payments/new"
       exact
     >Add Incoming/Outgoing Payments Related to this Account</NavLink>
-    </div> 
+    </div>
   );
 };
 
