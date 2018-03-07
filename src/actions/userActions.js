17478c096c06591ab6c97ea824a90c7b7e6d2f07
AdamT213 <adt6261@gmail.com>
2018-03-07 17:12:14 -0500

    fixed sign up action

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -3,14 +3,13 @@ import fetch from 'isomorphic-fetch'
 export function signUpUser(user){
     var data = {email: user.email, password: user.password, password_confirmation: user.password};
     return function(dispatch){
-        dispatch({type: 'SIGN_UP_USER'})
+        dispatch({type: 'SIGN_UP_USER', payload: user})
         return fetch('https://fidirect-api.herokuapp.com/api/users/sign_up', {
-        method: 'POST',
+        method: 'GET',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({user: data}),
         }).then(res => {
             return res.json()
           }).then(responseJson => {
